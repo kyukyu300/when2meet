bb076b8ea0a0ddc4423659a8c837db352dc56598
kyukyu300 <kyujin1252@gmail.com>
2025-05-28 07:21:27 +0900

    add get meeting spec

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.dtos.get_meeting_response import GetMeetingResponse
 from app.services.meeting_service_mysql import service_create_meeting_mysql
 
 mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
@@ -9,4 +10,9 @@ mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
 
 @mysql_router.post("", description="meeting을 생성합니다.")
 async def api_creating_meeting_mysql() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)
+    return CreateMeetingResponse(url_code="abc")
+
+
+@mysql_router.get("{meeting_url_code}", description="meeting을 조회합니다.")
+async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
