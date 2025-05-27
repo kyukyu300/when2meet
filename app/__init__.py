3b056a440297d644425bbf36065cf783fb6d56b3
kyukyu300 <kyujin1252@gmail.com>
2025-05-27 20:10:34 +0900

    install tortoise

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from app.apis.v1.meeting_router import mysql_router as meeting_mysql_router
+from app.configs.tortoise_config import initialize_tortoise
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
 app.include_router(meeting_mysql_router)
+
+initialize_tortoise(app)
--- test_temp.py
+++ test_temp.py
@@ -1,2 +1,13 @@
-def test_dummy() -> None:
-    assert 1 + 1 == 2
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def test_add() -> None:
+    # Given
+    a, b = 1, 1
+
+    # When
+    result = add(a, b)
+
+    # Then
+    assert result == 2
